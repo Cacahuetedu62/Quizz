c17df3e98b66ea69536e244af3f08df55ea7157a
Cacahuetedu62 <rogez.aurore01@gmail.com>
2024-10-10 13:59:50 +0200

    Stylisation du #result en cours + callback fait

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
-function submitQuiz(){
-// ici, avant il y avait, "console.log" de submitQuiz() test ok, quand on appuis sur soumettre 'coucou ca marche!" s'affiche = JS bien relié
-
-// création de la variable qui va déclarer les réponces correctes
-const correctAnswers = {
+function calculateScore(callback){
+    
+    // calculateScore => CALCUL création de la variable qui va déclarer les réponces correctes
+    const correctAnswers = {
         q1: "Paris",
         q2: "Mercure",
         q3: "Jupiter",
     }
-
-// création de la variable qui va récupérer le formulaire id="quiz-form"    
-const form = document.getElementById("quiz-form")
-
-// création de la variable qui va récupérer le score
-let score =0;
-
-//boucle pour chaque questions on va vérifié si la réponse choisie est ok avec le tableau : OK
-for(const question in correctAnswers){
+    
+    // création de la variable qui va récupérer le formulaire id="quiz-form"    
+    const form = document.getElementById("quiz-form")
+    
+    // création de la variable qui va récupérer le score
+    let score =0;
+    
+    //boucle pour chaque questions on va vérifié si la réponse choisie est ok avec le tableau : OK
+    for(const question in correctAnswers){
     const userAnswer = form[question].value
-if(userAnswer===correctAnswers[question]){
+    if(userAnswer===correctAnswers[question]){
     score++
-}
+    }}
+    
+    callback(score)
 }
 
-//recuperation du score : OK
+function displayResult(score, callback){
+//displayResult => AFFICHE recuperation du score : OK
 const resultDiv = document.getElementById("result")
-resultDiv.innerHTML = `Votre score est de ${score}`
+resultDiv.innerHTML = `Votre score est de ${score}`    
+callback(score)
+}
 
-//il fait une boucle pour affiché des commentaires : OK
+function handleMessage(score){
+//affiche un message en fonction du score
+const resultDiv = document.getElementById("result")
 if(score ===3){
     resultDiv.innerHTML += "<br> Excellent !"
+    //ajout d'une class css pour chaque
+    resultDiv.classList.add("excellent")
 } else if (score ===2){
     resultDiv.innerHTML += "<br> Bon ravail !"
+    resultDiv.classList.add("good")
 } else if (score ===1){
     resultDiv.innerHTML += "<br> Vous pouvez mieux faire !"
+    resultDiv.classList.add("tray-again")
 }
+}
+
+
+//submitQuiz => appel toutes les fonctions avec un callback
+function submitQuiz(){
+calculateScore(function(score){
+    displayResult(score,function (){
+        handleMessage(score)
+    })
+})
 }
